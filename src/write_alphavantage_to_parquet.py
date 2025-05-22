26feffca47b97eb722a3db8fc6b924d3a8ee517a
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-05-22 09:52:41 +0200

    build: create script to ingest api data from Alpha Vantage

---
+++ src/write_alphavantage_to_parquet.py
@@ -0,0 +1,28 @@
+'''
+This script fetches intraday stock data for IBM from
+the Alpha Vantage API and saves it as a Parquet file.
+'''
+
+# Import necessary libraries
+import pandas as pd
+import requests, json
+
+# Fetch data from Alpha Vantage API
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+# Convert the JSON data to a Pandas DataFrame
+ibm = pd.DataFrame(data['Time Series (5min)']).T
+
+# Get the current date from the intraday data
+current_date = pd.to_datetime('today').strftime('%Y_%m_%d')
+
+# Create a directory path to save the Parquet file
+sink_dir_path = "~/git_repos/project-anthill/data/"
+filename = f'ibm_{current_date}.parquet'
+save_path = sink_dir_path + filename
+print(f'Writing to {save_path}')
+
+# Write the DataFrame to a Parquet file
+ibm.to_parquet(save_path, index=True)
