c970613756bdfd5ad0bea2dbb2b0248e7bcee2bb
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-06 14:36:24 +0200

    feat: upgraded terraform version

--- terraform/environment/aws/dev/versions.tf
+++ terraform/environment/aws/dev/versions.tf
@@ -1,9 +1,13 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.7.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
