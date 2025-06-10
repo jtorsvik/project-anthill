01c497bed7c84b3776e3f729ba871a9514b5ae7c
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-10 11:27:12 +0200

    fix: added environment variable on the aws bucket module

--- terraform/environment/aws/dev/main.tf
+++ terraform/environment/aws/dev/main.tf
@@ -4,5 +4,5 @@
 module "aws_s3_bucket" {
     source        = "../../../modules/aws/storage/s3_bucket"
     # version     = "1.0.0"
-    bucket_name   = "project-anthill-s3-bucket-tf-${env}"
+    bucket_name   = "project-anthill-s3-bucket-tf-${var.env}"
 }
