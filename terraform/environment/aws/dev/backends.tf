0f0cceafb0f69dd056841c20f11a5beb8ac0dabe
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-10 13:21:13 +0200

    fix: moved backend tfstatein s3  into a separate backends.tf file

---
+++ terraform/environment/aws/dev/backends.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+      bucket         = "project-anthill-s3-tfstate-dev"
+      key            = "state/terraform.tfstate"
+      region         = "eu-west-1"
+      use_lockfile   = true
+    }
+  }
