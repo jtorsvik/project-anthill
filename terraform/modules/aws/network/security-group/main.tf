92206c3c2962a33e85b24cce926239e6f07b0409
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-16 11:54:58 +0200

    build: security group egress

--- terraform/modules/aws/network/security-group/main.tf
+++ terraform/modules/aws/network/security-group/main.tf
@@ -12,8 +12,16 @@ resource "aws_security_group" "this" {
 
 resource "aws_vpc_security_group_ingress_rule" "this" {
   security_group_id = aws_security_group.this.id
-  cidr_ipv4         = var.cidr_ipv4
-  to_port           = var.to_port
-  from_port         = var.from_port
-  ip_protocol       = var.ip_protocol
+  cidr_ipv4         = var.ingress_cidr_ipv4
+  to_port           = var.ingress_to_port
+  from_port         = var.ingress_from_port
+  ip_protocol       = var.ingress_ip_protocol
+}
+
+resource "aws_vpc_security_group_egress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = var.egress_cidr_ipv4
+  to_port           = var.egress_to_port
+  from_port         = var.egress_from_port
+  ip_protocol       = var.egress_ip_protocol
 }
