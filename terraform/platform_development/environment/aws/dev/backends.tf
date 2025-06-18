a71698c958e083c41b2000d8774b093be5ae8469
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-18 10:47:42 +0200

    fix: copy all files except locals.tf and backends.tf

--- terraform/platform_development/environment/aws/dev/backends.tf
+++ terraform/platform_development/environment/aws/dev/backends.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "project-anthill-s3-tfstate-${var.env}"
+    bucket       = "project-anthill-s3-tfstate-dev"
     key          = "platform/state/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
--- terraform/platform_development/environment/aws/test/backends.tf
+++ terraform/platform_development/environment/aws/test/backends.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "project-anthill-s3-tfstate-${var.env}"
+    bucket       = "project-anthill-s3-tfstate-test"
     key          = "platform/state/terraform.tfstate"
     region       = "eu-west-1"
   }
