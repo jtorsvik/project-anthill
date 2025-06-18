5804aa9ed05fa980e0400388af054abf0e98093e
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-18 10:24:15 +0200

    fix: terraform backends

--- terraform/platform_development/environment/aws/dev/backends.tf
+++ terraform/platform_development/environment/aws/dev/backends.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "project-anthill-s3-tfstate-dev"
+    bucket       = "project-anthill-s3-tfstate-${var.env}"
     key          = "platform/state/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
