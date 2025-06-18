b79404f530c0f3f0d3f6d32dcb2f2a07ecf0470d
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-18 12:09:44 +0200

    fix: commented out use_lockfile in backends.tf in dev

--- terraform/platform_development/environment/aws/dev/backends.tf
+++ terraform/platform_development/environment/aws/dev/backends.tf
@@ -3,6 +3,6 @@ terraform {
     bucket       = "project-anthill-s3-tfstate-dev"
     key          = "platform/state/terraform.tfstate"
     region       = "eu-west-1"
-    use_lockfile = true
+    # use_lockfile = true
   }
 }
