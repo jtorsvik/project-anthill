996df5261e9c5b4a7354399b4c960e83dd3ffd40
TORSVIK Joakim Martin <jm.torsvik@soprasteria.com>
2025-06-18 11:40:46 +0200

    fix: moved enviornment variables into a separate variables.tf file

---
+++ terraform/platform_development/environment/aws/dev/env-variables.tf
@@ -0,0 +1,27 @@
+# ----------------------------------------
+# Environment Variables for AWS Resources
+# ----------------------------------------
+
+variable "env" {
+  description = "The environment for which the resources are being created (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "owner" {
+  description = "The owner of the resources"
+  type        = string
+  default     = "joakimmt-admin"
+}
+
+variable "project" {
+  description = "The project name for which the resources are being created"
+  type        = string
+  default     = "project-anthill"
+}
+
+variable "region" {
+  description = "The AWS region where the resources will be created"
+  type        = string
+  default     = "eu-west-1"
+}
